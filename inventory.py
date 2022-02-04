a840fb782c8afdbb4cd5277d21bc307791288328
Fernando Kravosac <97983929+Kravosac@users.noreply.github.com>
2022-02-04 10:16:45 -0300

    Third commit

--- inventory.py
+++ inventory.py
@@ -1,4 +1,3 @@
-
 file1 = 'inventory.txt'
 
 
@@ -9,45 +8,65 @@ def menu():
     print('[3] - General report')
     print('[4] - Unavailable products report')
     print('[5] - Exit system\n')
-    option = int(input('Chose an option: '))
-    if option == 1:
+    option = input('Chose an option: ')
+    while option != '1' and option != '2' and option != '3' and option != '4' and option != '5':
+        option = input('Please enter a valid value: ')
+    if option == '1':
         return register()
-    elif option == 2:
+    elif option == '2':
         return quantity()
-    elif option == 3:
+    elif option == '3':
         return general_report()
-    elif option == 4:
+    elif option == '4':
         return unavailable()
-    elif option == 5:
+    elif option == '5':
         print('\n*** End of execution ***\n')
 
 
 def register():
-
     with open(file1, 'a+', encoding='UTF-8') as file:
         print('\n*** INSERT NEW PRODUCT ***\n')
 
         while True:
 
             code = input('Product code: ')
+            while len(code) == 0 or len(code) > 6:
+                code = input('Please enter a valid code (up to 6 characters): ')
             while content(code) is True:
                 code = input('Code already registered, please try a new one: ')
-            file.write(f'{code} ')
 
             prod = input('Product name: ')
+            while len(prod) == 0 or len(prod) > 20:
+                prod = input('Please enter a valid product (up to 20 characters): ')
             while content(prod) is True:
                 prod = input('Product already registered, please try a new one: ')
-            file.write(f'{prod} ')
 
-            quant = int(input('Quantity: '))
+            try:
+                quant = int(input('Quantity: '))
+            except ValueError:
+                print('Invalid value')
+                break
+
+            file.write(f'{code} ')
+            file.write(f'{prod} ')
             file.write(f'{quant}\n')
 
             option = input('Register another product? (y/n): ')
+            while option.lower() != 'y' and option.lower() != 'n':
+                option = input('Please type only "y" or "n": ')
             if option == 'n':
-                print('\nProduct(s) registered sussesfully')
+                print('\nProduct(s) registered sussesfully!')
                 break
 
-    return menu()
+        print('\n[0] - Return to main menu')
+        print('[1] - Exit system')
+        option = input('\nChose an option: ')
+        while option != '0' and option != '1':
+            option = int(input('Please enter a valid value: '))
+        if option == '0':
+            return menu()
+        elif option == '1':
+            print('\n*** End of execution ***\n')
 
 
 def quantity():
@@ -55,18 +74,43 @@ def quantity():
     lst = dic()
     print('\n*** INPUT OR REMOVE QUANTITY ***\n')
     prod = input('Product: ')
-    quant = int(input('Please enter the quantity (include " - " to remove): '))
-    for i in range(len(lst)):
-        if lst[i]['product'] == prod:
-            lst[i]['quantity'] += quant
-            print(f"\nNew quantity of {lst[i]['product']} = {lst[i]['quantity']}")
+    if content(prod) is False:
+        print('\nProduct not found\n')
+        print('[0] - General report')
+        print('[1] - Exit system')
+        option = input('\nChose an option: ')
+        while option != '0' and option != '1':
+            option = int(input('Please enter a valid value: '))
+        if option == '0':
+            return general_report()
+        elif option == '1':
+            print('\n*** End of execution ***\n')
+    else:
+        try:
+            quant = int(input('Please enter the quantity (include " - " to remove): '))
+            for i in range(len(lst)):
+                if lst[i]['product'] == prod:
+                    lst[i]['quantity'] += quant
+                    print(f"\nNew quantity of {lst[i]['product']} = {lst[i]['quantity']}")
 
-    with open(file1, 'w', encoding='UTF-8') as file:
-        file.truncate(0)
-        for i in range(len(lst)):
-            file.write(f"{lst[i]['code']} {lst[i]['product']} {lst[i]['quantity']}\n")
+            with open(file1, 'w', encoding='UTF-8') as file:
+                file.truncate(0)
+                for i in range(len(lst)):
+                    file.write(f"{lst[i]['code']} {lst[i]['product']} {lst[i]['quantity']}\n")
 
-    return menu()
+            return menu()
+
+        except ValueError:
+            print('\nInvalid value')
+            print('\n[0] - Return to main menu')
+            print('[1] - Exit system')
+            option = input('\nChose an option: ')
+            while option != '0' and option != '1':
+                option = int(input('Please enter a valid value: '))
+            if option == '0':
+                return menu()
+            elif option == '1':
+                print('\n*** End of execution ***\n')
 
 
 def content(word):
@@ -84,16 +128,16 @@ def dic():
         file.seek(0)
         lst = (file.read()).split()
         new_lst = []
-        for i in range(0, len(lst)-2, 3):
+        for i in range(0, len(lst) - 2, 3):
             di = {}
             key = 'code'
             value = lst[i]
             di.update({key: value})
             key = 'product'
-            value = lst[i+1]
+            value = lst[i + 1]
             di.update({key: value})
             key = 'quantity'
-            value = int(lst[i+2])
+            value = int(lst[i + 2])
             di.update({key: value})
             new_lst.append(di)
         return new_lst
